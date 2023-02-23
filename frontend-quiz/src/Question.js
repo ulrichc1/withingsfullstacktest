a883c82093c0bcb8bdd8a722cf3cb203ab3f4714
ulrichc1 <coudinulrich@gmail.com>
2023-02-23 17:09:52 +0100

    question.js addition

--- frontend-quiz/src/Question.js
+++ frontend-quiz/src/Question.js
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
 const Question = (props) => {
-	const [selected, setSelected] = useState(null) // TODO select an answer
+    const [selected, setSelected] = useState(null); // Selected Answer
 
-	const handleClick = () => {
-		// TODO check answer selected and call next question
-		// TODO highlight good answer
-	};
 
-	useEffect(() => {
-		// TODO reset when question change
-	}, []);
+const handleClick = () => {
+    // Check if an answer is selected and call the next question then
+    if (selected === null) {
+        alert("Please select an answer !");
+        return;
+    } else {
+        props.nextQuestion();
+    }
+    // Highlights the correct answer
+    if (selected === props.selectedAnswer) {
+        document.querySelector(".question-answers p:nth-child(" + (props.selectedAnswer + 1) + ")").classList.add("correct");
+    }
+};
 
-	return (
-		<div className="question-container">
-			<p>Question title</p>
-			<div className="question-answers">
-				<p>Answer 1</p>
-				<p>Answer 2</p>
-				<p>Answer 3</p>
-				<p>Answer 4</p>
-			</div>
-			<button onClick={handleClick}>Validate</button>
-		</div>
-	)
+// Reset selected answer when a new question is shown
+useEffect(() => {
+    setSelected(null);
+}, [props.question]);
+
+return (
+    <div className="question-container">
+        <p>{props.question}</p>
+        <div className="question-answers">
+            <p onClick={() => setSelected(0)}>Answer 1</p>
+            <p onClick={() => setSelected(1)}>Answer 2</p>
+            <p onClick={() => setSelected(2)}>Answer 3</p>
+            <p onClick={() => setSelected(3)}>Answer 4</p>
+        </div>
+        <button onClick={handleClick}>Validate</button>
+    </div>
+)
+
 }
+
 
 export default Question;
